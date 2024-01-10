4ecaca2cc5a7bbb06ca9570aa421c7c3582ba881
nozawamitsuki <kaminagara13@gmail.com>
2024-01-10 22:11:26 +0900

    お問い合わせフォームの編集

--- js/script.js
+++ js/script.js
@@ -23,3 +23,53 @@ $(window).on("load" ,function(){
   $('.message').fadeIn(4000);
 });
 
+// -----------
+// お問い合わせフォームの設定
+// ------------
+
+
+const button = document.getElementById('check-btn');
+button.addEventListener('click', validation);
+
+function validation(event) {
+    event.preventDefault(); // フォームのデフォルトの送信を防止
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const selectBox = document.getElementById("selectBox"); 
+    const output = document.getElementById('output');
+    let errors = [];
+
+    if (name.trim() === '') {
+        errors.push('お名前を入力してください。');
+    } else if (name.length > 10) {
+        errors.push('お名前が10文字を超えています。');
+    }
+
+    const emailPattern = /^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+$/;
+    if (!emailPattern.test(email)) {
+        errors.push('メールアドレスの入力形式が正しくありません。');
+    }
+    
+    const alertAreaSelect = document.getElementById('alertAreaSelect');
+    if(selectBox.options[0].selected === true) {
+      errors.push('お問い合わせ内容の選択は必須です');
+    };
+  
+  selectBox.addEventListener('change', () => {
+    const alertAreaSelect = document.getElementById('alertAreaSelect');
+    if(selectBox.options[0].selected === false) {
+      errors.push('お問い合わせ内容の選択は必須です');
+    }
+  });
+
+    if (errors.length > 0) {
+        output.innerHTML = errors.join('<br>');
+    } else {
+        output.innerHTML = '';
+        alert('入力内容を確認しました。');
+        // バリデーションがOKならフォームを送信
+        document.getElementById('validationForm').submit();
+    }
+    
+}
